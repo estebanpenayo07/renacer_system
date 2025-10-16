3916762e263c80038237714182b2f873d050aeb5
Esteban <estebanpenayo18@hotmail.com>
2025-10-16 20:51:43 -0300

    Primer commit

---
+++ app/javascript/application.js
@@ -0,0 +1,7 @@
+// Entry point for the build script in your package.json
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import "@hotwired/turbo-rails"
+import "./controllers"
+
+
